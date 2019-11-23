9c89cf66c97840bb50e4ea00ff9f7e8fd8deacac
Masayuki Ono <mono0926@gmail.com>
2019-11-23 19:14:41 +0900

    Add DraggableScrollableSheet sample

---
+++ lib/main_draggable_scrollable_sheet.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DraggableScrollableSheet'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topCenter,
+            child: Image.network('https://mono0926.com/images/love_logo.png'),
+          ),
+          DraggableScrollableSheet(
+            initialChildSize: 0.5,
+            minChildSize: 0.2,
+            maxChildSize: 0.9,
+            builder: (context, scrollController) {
+              return Container(
+                color: Colors.blue[100],
+                child: ListView.builder(
+                  controller: scrollController,
+                  itemCount: 25,
+                  itemBuilder: (context, index) {
+                    return ListTile(title: Text('Item $index'));
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
