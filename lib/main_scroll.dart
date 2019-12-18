a9859cbda65497962dc11925eb5b262a5a99b150
Masayuki Ono <mono0926@gmail.com>
2019-12-19 06:39:46 +0900

    Refactor

--- lib/main_scroll.dart
+++ lib/main_scroll.dart
@@ -33,6 +33,12 @@ class __BodyState extends State<_Body> {
   final _horizontalScrollReceiver = ScrollController();
   final _horizontalScrollSender = ScrollController();
 
+  static const cellWidth = 80.0;
+  static const cellHeight = 60.0;
+  static const axisWidth = 40.0;
+  static const colCount = 20;
+  static const rowCount = 40;
+
   @override
   void initState() {
     super.initState();
@@ -57,88 +63,99 @@ class __BodyState extends State<_Body> {
 
   @override
   Widget build(BuildContext context) {
-    const cellWidth = 80.0;
-    const cellHeight = 60.0;
-    const axisWidth = 40.0;
-    const colCount = 20;
-    const rowCount = 40;
     return Column(
       children: <Widget>[
         SizedBox(
           height: axisWidth,
           child: Row(
             children: <Widget>[
-              Container(
-                width: axisWidth,
-                color: Colors.grey,
-              ),
-              Expanded(
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  controller: _horizontalScrollSender,
-                  children: List.generate(
-                    colCount,
-                    (i) => const Placeholder(
-                      fallbackWidth: cellWidth,
-                      color: Colors.red,
-                    ),
-                  ),
-                ),
-              ),
+              _buildCorner(),
+              _buildXLabels(),
             ],
           ),
         ),
         Expanded(
           child: Row(
             children: <Widget>[
-              SizedBox(
-                width: axisWidth,
-                child: ListView(
-                  scrollDirection: Axis.vertical,
-                  controller: _verticalScrollSender,
-                  children: List.generate(
-                    rowCount,
-                    (i) => const Placeholder(
-                      fallbackHeight: cellHeight,
-                      color: Colors.blue,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: SizedBox.expand(
-                  child: SingleChildScrollView(
-                    physics: const AlwaysScrollableScrollPhysics(),
-                    controller: _verticalScrollReceiver,
-                    child: SizedBox(
-                      height: cellHeight * rowCount,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        controller: _horizontalScrollReceiver,
-                        physics: const AlwaysScrollableScrollPhysics(),
-                        children: List.generate(
-                          colCount,
-                          (i) => SizedBox(
-                            width: cellWidth,
-                            child: Column(
-                              children: List.generate(
-                                rowCount,
-                                (i) => const Placeholder(
-                                  fallbackHeight: cellHeight,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              )
+              _buildYLabels(),
+              _buildGrid(),
             ],
           ),
         )
       ],
+    );
+  }
+
+  Widget _buildCorner() {
+    return Container(
+      width: axisWidth,
+      color: Colors.grey,
+    );
+  }
+
+  Widget _buildXLabels() {
+    return Expanded(
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        controller: _horizontalScrollSender,
+        children: List.generate(
+          colCount,
+          (i) => const Placeholder(
+            fallbackWidth: cellWidth,
+            color: Colors.red,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildYLabels() {
+    return SizedBox(
+      width: axisWidth,
+      child: ListView(
+        scrollDirection: Axis.vertical,
+        controller: _verticalScrollSender,
+        children: List.generate(
+          rowCount,
+          (i) => const Placeholder(
+            fallbackHeight: cellHeight,
+            color: Colors.blue,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildGrid() {
+    return Expanded(
+      child: SizedBox.expand(
+        child: SingleChildScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          controller: _verticalScrollReceiver,
+          child: SizedBox(
+            height: cellHeight * rowCount,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              controller: _horizontalScrollReceiver,
+              physics: const AlwaysScrollableScrollPhysics(),
+              children: List.generate(
+                colCount,
+                (i) => SizedBox(
+                  width: cellWidth,
+                  child: Column(
+                    children: List.generate(
+                      rowCount,
+                      (i) => const Placeholder(
+                        fallbackHeight: cellHeight,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
