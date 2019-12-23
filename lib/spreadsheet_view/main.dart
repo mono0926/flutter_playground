b88d43b09e4ecb87a44404a1b7945537a06e04c9
Masayuki Ono <mono0926@gmail.com>
2019-12-23 16:19:50 +0900

    Add SpreadsheetView

---
+++ lib/spreadsheet_view/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import 'spreadsheet_view.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Bidirectional Scroll Page')),
+      body: SpreadsheetView(
+        columnHeaderHeight: 40,
+        rowHeaderWidth: 50,
+        cellWidth: 80,
+        cellHeight: 40,
+        columnCount: 20,
+        rowCount: 40,
+        corner: const Placeholder(),
+        columnHeaderBuilder: (context, i) => Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.red[100],
+            border: Border.all(
+              color: Colors.black,
+            ),
+          ),
+          child: Text('Col $i'),
+        ),
+        rowHeaderBuilder: (context, i) => Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.red[100],
+            border: Border.all(
+              color: Colors.black,
+            ),
+          ),
+          child: Text('Row $i'),
+        ),
+        cellBuilder: (context, row, column) {
+          return Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.grey,
+              ),
+            ),
+            child: Text('$row-$column'),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/spreadsheet_view/spreadsheet_view.dart
@@ -0,0 +1,162 @@
+//import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+typedef HeaderBuilder = Widget Function(BuildContext context, int index);
+typedef CellBuilder = Widget Function(
+  BuildContext context,
+  int row,
+  int column,
+);
+
+class SpreadsheetView extends StatefulWidget {
+  const SpreadsheetView({
+    Key key,
+    @required this.corner,
+    @required this.columnHeaderBuilder,
+    @required this.rowHeaderBuilder,
+    @required this.cellBuilder,
+    @required this.columnHeaderHeight,
+    @required this.rowHeaderWidth,
+    @required this.cellWidth,
+    @required this.cellHeight,
+    @required this.columnCount,
+    @required this.rowCount,
+  }) : super(key: key);
+
+  final Widget corner;
+  final HeaderBuilder columnHeaderBuilder;
+  final HeaderBuilder rowHeaderBuilder;
+  final CellBuilder cellBuilder;
+  final double columnHeaderHeight;
+  final double rowHeaderWidth;
+  final double cellWidth;
+  final double cellHeight;
+  final int columnCount;
+  final int rowCount;
+
+  @override
+  _SpreadsheetViewState createState() => _SpreadsheetViewState();
+}
+
+class _SpreadsheetViewState extends State<SpreadsheetView> {
+  final _verticalScrollReceiver = ScrollController();
+  final _verticalScrollSender = ScrollController();
+  final _horizontalScrollReceiver = ScrollController();
+  final _horizontalScrollSender = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _verticalScrollReceiver.addListener(_updateAxisVerticalOffset);
+    _horizontalScrollReceiver.addListener(_updateAxisHorizontalOffset);
+  }
+
+  @override
+  void dispose() {
+    _verticalScrollReceiver.removeListener(_updateAxisVerticalOffset);
+    _horizontalScrollReceiver.removeListener(_updateAxisHorizontalOffset);
+    super.dispose();
+  }
+
+  void _updateAxisVerticalOffset() {
+    _verticalScrollSender.jumpTo(_verticalScrollReceiver.offset);
+  }
+
+  void _updateAxisHorizontalOffset() {
+    _horizontalScrollSender.jumpTo(_horizontalScrollReceiver.offset);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: widget.columnHeaderHeight,
+          child: Row(
+            children: <Widget>[
+              _buildCorner(),
+              Expanded(
+                child: _buildColumnLabels(),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Row(
+            children: <Widget>[
+              SizedBox(
+                child: _buildYLabels(),
+                width: widget.rowHeaderWidth,
+              ),
+              Expanded(
+                child: _buildGrid(),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildCorner() {
+    return SizedBox(
+      width: widget.rowHeaderWidth,
+      child: widget.corner,
+    );
+  }
+
+  Widget _buildColumnLabels() {
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      controller: _horizontalScrollSender,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: widget.columnCount,
+      itemBuilder: (context, index) => SizedBox(
+        width: widget.cellWidth,
+        child: widget.columnHeaderBuilder(context, index),
+      ),
+    );
+  }
+
+  Widget _buildYLabels() {
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      controller: _verticalScrollSender,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: widget.rowCount,
+      itemBuilder: (context, index) => SizedBox(
+        height: widget.cellHeight,
+        child: widget.rowHeaderBuilder(context, index),
+      ),
+    );
+  }
+
+  Widget _buildGrid() {
+    return SingleChildScrollView(
+      physics: const AlwaysScrollableScrollPhysics(),
+      controller: _verticalScrollReceiver,
+      child: SizedBox(
+        height: widget.cellHeight * widget.rowCount,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          controller: _horizontalScrollReceiver,
+          physics: const AlwaysScrollableScrollPhysics(),
+          itemCount: widget.columnCount,
+          itemBuilder: (context, column) => SizedBox(
+            width: widget.cellWidth,
+            child: Column(
+              children: List.generate(
+                widget.rowCount,
+                (row) => SizedBox(
+                  height: widget.cellHeight,
+                  child: widget.cellBuilder(context, row, column),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
