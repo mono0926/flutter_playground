6a07941557d39ca90cb6b65dba8e3941099091fe
Masayuki Ono <mono0926@gmail.com>
2020-01-27 07:15:32 +0900

    Add dialog example

---
+++ lib/main_dialog.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog<void>(
+            context: context,
+            builder: (context) {
+              final mediaQuery = MediaQuery.of(context);
+              return MediaQuery(
+                data: mediaQuery.copyWith(
+                  viewInsets: mediaQuery.viewInsets +
+                      const EdgeInsets.symmetric(horizontal: 100),
+                ),
+                child: const AlertDialog(
+                  content: Text('Hello Dialog'),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
