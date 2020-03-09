9888fa2bffa7f1e9d4f8ef6d5e11f2d4498b851c
Masayuki Ono <mono0926@gmail.com>
2020-03-09 17:29:55 +0900

    Add main_state_notifier2

---
+++ lib/main_state_notifier2.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:provider/provider.dart';
+import 'package:state_notifier/state_notifier.dart';
+
+part 'main_state_notifier2.freezed.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MultiProvider(
+        providers: [
+          StateNotifierProvider<_Controller1, Counter1>(
+            create: (context) => _Controller1(),
+          ),
+          StateNotifierProvider<_Controller2, Counter2>(
+            create: (context) => _Controller2(),
+          ),
+        ],
+        child: const HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          '${context.select((Counter2 c) => c.count)}',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: context.watch<_Controller2>().increment,
+      ),
+    );
+  }
+}
+
+class _Controller1 extends StateNotifier<Counter1> {
+  _Controller1() : super(const Counter1(count: 0));
+
+  void increment() => state = state.copyWith(
+        count: state.count + 1,
+      );
+}
+
+@freezed
+abstract class Counter1 with _$Counter1 {
+  const factory Counter1({@required int count}) = _Counter1;
+}
+
+class _Controller2 extends StateNotifier<Counter2> with LocatorMixin {
+  _Controller2() : super(const Counter2(count: 0));
+
+  void increment() => state = state.copyWith(
+        count: state.count + 1,
+      );
+
+  @override
+  void update(T Function<T>() watch) {
+    // If uncomment this, error occurs:
+    // 'package:provider/src/provider.dart': Failed assertion: line 427 pos 12: '!debugDoingBuild && !_debugIsInInheritedProviderUpdate': is not true.
+//    final counter1 = watch<Counter1>();
+//    print(counter1);
+
+    // OK:
+    final controller1 = watch<_Controller1>();
+    print(controller1);
+  }
+}
+
+@freezed
+abstract class Counter2 with _$Counter2 {
+  const factory Counter2({@required int count}) = _Counter2;
+}
---
+++ lib/main_state_notifier2.freezed.dart
@@ -0,0 +1,146 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named
+
+part of 'main_state_notifier2.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+mixin _$Counter1 {
+  int get count;
+
+  Counter1 copyWith({int count});
+}
+
+class _$Counter1TearOff {
+  const _$Counter1TearOff();
+
+  _Counter1 call({@required int count}) {
+    return _Counter1(
+      count: count,
+    );
+  }
+}
+
+const $Counter1 = _$Counter1TearOff();
+
+class _$_Counter1 with DiagnosticableTreeMixin implements _Counter1 {
+  const _$_Counter1({@required this.count}) : assert(count != null);
+
+  @override
+  final int count;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'Counter1(count: $count)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'Counter1'))
+      ..add(DiagnosticsProperty('count', count));
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _Counter1 &&
+            (identical(other.count, count) ||
+                const DeepCollectionEquality().equals(other.count, count)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^ const DeepCollectionEquality().hash(count);
+
+  @override
+  _$_Counter1 copyWith({
+    Object count = freezed,
+  }) {
+    return _$_Counter1(
+      count: count == freezed ? this.count : count as int,
+    );
+  }
+}
+
+abstract class _Counter1 implements Counter1 {
+  const factory _Counter1({@required int count}) = _$_Counter1;
+
+  @override
+  int get count;
+
+  @override
+  _Counter1 copyWith({int count});
+}
+
+mixin _$Counter2 {
+  int get count;
+
+  Counter2 copyWith({int count});
+}
+
+class _$Counter2TearOff {
+  const _$Counter2TearOff();
+
+  _Counter2 call({@required int count}) {
+    return _Counter2(
+      count: count,
+    );
+  }
+}
+
+const $Counter2 = _$Counter2TearOff();
+
+class _$_Counter2 with DiagnosticableTreeMixin implements _Counter2 {
+  const _$_Counter2({@required this.count}) : assert(count != null);
+
+  @override
+  final int count;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'Counter2(count: $count)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'Counter2'))
+      ..add(DiagnosticsProperty('count', count));
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _Counter2 &&
+            (identical(other.count, count) ||
+                const DeepCollectionEquality().equals(other.count, count)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^ const DeepCollectionEquality().hash(count);
+
+  @override
+  _$_Counter2 copyWith({
+    Object count = freezed,
+  }) {
+    return _$_Counter2(
+      count: count == freezed ? this.count : count as int,
+    );
+  }
+}
+
+abstract class _Counter2 implements Counter2 {
+  const factory _Counter2({@required int count}) = _$_Counter2;
+
+  @override
+  int get count;
+
+  @override
+  _Counter2 copyWith({int count});
+}
