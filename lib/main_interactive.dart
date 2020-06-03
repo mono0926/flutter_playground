639e9e278e02a0c6c76c7755e34a60eab3b65311
Masayuki Ono <mono0926@gmail.com>
2020-06-03 10:03:03 +0900

    Add InteractiveViewer sample

---
+++ lib/main_interactive.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: InteractiveViewer(
+          boundaryMargin: const EdgeInsets.all(20),
+          minScale: 0.1,
+          maxScale: 1.6,
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: <Color>[Colors.orange, Colors.red],
+                stops: <double>[0, 1],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
