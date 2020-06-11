4155316a1f1c9564635f2a0da85d0be52610e0e1
Masayuki Ono <mono0926@gmail.com>
2020-06-11 15:15:31 +0900

    Revert "Wrap by Padding"
    This reverts commit e154001b7c9db96bd48f44f2567a72822034eb19.

--- lib/main_diff.dart
+++ lib/main_diff.dart
@@ -17,13 +17,10 @@ class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8),
-      child: Scaffold(
-        appBar: AppBar(),
-        body: const Center(
-          child: Text('Dog'),
-        ),
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Dog'),
       ),
     );
   }
