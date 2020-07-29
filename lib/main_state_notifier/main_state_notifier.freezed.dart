fed3f496bc15d706a65c0ff6170bdf97f77f2caf
Masayuki Ono <mono0926@gmail.com>
2020-07-29 16:42:18 +0900

    UPdate freezed

--- lib/main_state_notifier/main_state_notifier.freezed.dart
+++ lib/main_state_notifier/main_state_notifier.freezed.dart
@@ -1,5 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named
+// ignore_for_file: deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies
 
 part of 'main_state_notifier.dart';
 
@@ -7,15 +7,12 @@ part of 'main_state_notifier.dart';
 // FreezedGenerator
 // **************************************************************************
 
-mixin _$Counter {
-  int get count;
-
-  Counter copyWith({int count});
-}
+T _$identity<T>(T value) => value;
 
 class _$CounterTearOff {
   const _$CounterTearOff();
 
+// ignore: unused_element
   _Counter call({@required int count}) {
     return _Counter(
       count: count,
@@ -23,7 +20,62 @@ class _$CounterTearOff {
   }
 }
 
+// ignore: unused_element
 const $Counter = _$CounterTearOff();
+
+mixin _$Counter {
+  int get count;
+
+  $CounterCopyWith<Counter> get copyWith;
+}
+
+abstract class $CounterCopyWith<$Res> {
+  factory $CounterCopyWith(Counter value, $Res Function(Counter) then) =
+      _$CounterCopyWithImpl<$Res>;
+  $Res call({int count});
+}
+
+class _$CounterCopyWithImpl<$Res> implements $CounterCopyWith<$Res> {
+  _$CounterCopyWithImpl(this._value, this._then);
+
+  final Counter _value;
+  // ignore: unused_field
+  final $Res Function(Counter) _then;
+
+  @override
+  $Res call({
+    Object count = freezed,
+  }) {
+    return _then(_value.copyWith(
+      count: count == freezed ? _value.count : count as int,
+    ));
+  }
+}
+
+abstract class _$CounterCopyWith<$Res> implements $CounterCopyWith<$Res> {
+  factory _$CounterCopyWith(_Counter value, $Res Function(_Counter) then) =
+      __$CounterCopyWithImpl<$Res>;
+  @override
+  $Res call({int count});
+}
+
+class __$CounterCopyWithImpl<$Res> extends _$CounterCopyWithImpl<$Res>
+    implements _$CounterCopyWith<$Res> {
+  __$CounterCopyWithImpl(_Counter _value, $Res Function(_Counter) _then)
+      : super(_value, (v) => _then(v as _Counter));
+
+  @override
+  _Counter get _value => super._value as _Counter;
+
+  @override
+  $Res call({
+    Object count = freezed,
+  }) {
+    return _then(_Counter(
+      count: count == freezed ? _value.count : count as int,
+    ));
+  }
+}
 
 class _$_Counter with DiagnosticableTreeMixin implements _Counter {
   const _$_Counter({@required this.count}) : assert(count != null);
@@ -57,13 +109,8 @@ class _$_Counter with DiagnosticableTreeMixin implements _Counter {
       runtimeType.hashCode ^ const DeepCollectionEquality().hash(count);
 
   @override
-  _$_Counter copyWith({
-    Object count = freezed,
-  }) {
-    return _$_Counter(
-      count: count == freezed ? this.count : count as int,
-    );
-  }
+  _$CounterCopyWith<_Counter> get copyWith =>
+      __$CounterCopyWithImpl<_Counter>(this, _$identity);
 }
 
 abstract class _Counter implements Counter {
@@ -71,7 +118,6 @@ abstract class _Counter implements Counter {
 
   @override
   int get count;
-
   @override
-  _Counter copyWith({int count});
+  _$CounterCopyWith<_Counter> get copyWith;
 }
