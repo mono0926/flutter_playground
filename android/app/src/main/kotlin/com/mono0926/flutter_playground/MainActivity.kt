13f9bf870ce98f25271a700414702a3003cd9e88
Masayuki Ono <mono0926@gmail.com>
2020-08-04 14:22:09 +0900

    Run flutter create

--- android/app/src/main/kotlin/com/mono0926/flutter_playground/MainActivity.kt
+++ android/app/src/main/kotlin/com/mono0926/flutter_playground/MainActivity.kt
@@ -1,13 +1,6 @@
 package com.mono0926.flutter_playground
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
 }
