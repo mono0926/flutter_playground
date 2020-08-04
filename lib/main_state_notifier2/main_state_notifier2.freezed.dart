be88b9113b5e7f0f12edd9ad724d8bcd81b19ad5
Masayuki Ono <mono0926@gmail.com>
2020-08-04 14:09:12 +0900

    Update freezed

--- lib/main_state_notifier2/main_state_notifier2.freezed.dart
+++ lib/main_state_notifier2/main_state_notifier2.freezed.dart
@@ -1,5 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named
+// ignore_for_file: deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies
 
 part of 'main_state_notifier2.dart';
 
@@ -7,15 +7,12 @@ part of 'main_state_notifier2.dart';
 // FreezedGenerator
 // **************************************************************************
 
-mixin _$Counter1 {
-  int get count;
-
-  Counter1 copyWith({int count});
-}
+T _$identity<T>(T value) => value;
 
 class _$Counter1TearOff {
   const _$Counter1TearOff();
 
+// ignore: unused_element
   _Counter1 call({@required int count}) {
     return _Counter1(
       count: count,
@@ -23,7 +20,62 @@ class _$Counter1TearOff {
   }
 }
 
+// ignore: unused_element
 const $Counter1 = _$Counter1TearOff();
+
+mixin _$Counter1 {
+  int get count;
+
+  $Counter1CopyWith<Counter1> get copyWith;
+}
+
+abstract class $Counter1CopyWith<$Res> {
+  factory $Counter1CopyWith(Counter1 value, $Res Function(Counter1) then) =
+      _$Counter1CopyWithImpl<$Res>;
+  $Res call({int count});
+}
+
+class _$Counter1CopyWithImpl<$Res> implements $Counter1CopyWith<$Res> {
+  _$Counter1CopyWithImpl(this._value, this._then);
+
+  final Counter1 _value;
+  // ignore: unused_field
+  final $Res Function(Counter1) _then;
+
+  @override
+  $Res call({
+    Object count = freezed,
+  }) {
+    return _then(_value.copyWith(
+      count: count == freezed ? _value.count : count as int,
+    ));
+  }
+}
+
+abstract class _$Counter1CopyWith<$Res> implements $Counter1CopyWith<$Res> {
+  factory _$Counter1CopyWith(_Counter1 value, $Res Function(_Counter1) then) =
+      __$Counter1CopyWithImpl<$Res>;
+  @override
+  $Res call({int count});
+}
+
+class __$Counter1CopyWithImpl<$Res> extends _$Counter1CopyWithImpl<$Res>
+    implements _$Counter1CopyWith<$Res> {
+  __$Counter1CopyWithImpl(_Counter1 _value, $Res Function(_Counter1) _then)
+      : super(_value, (v) => _then(v as _Counter1));
+
+  @override
+  _Counter1 get _value => super._value as _Counter1;
+
+  @override
+  $Res call({
+    Object count = freezed,
+  }) {
+    return _then(_Counter1(
+      count: count == freezed ? _value.count : count as int,
+    ));
+  }
+}
 
 class _$_Counter1 with DiagnosticableTreeMixin implements _Counter1 {
   const _$_Counter1({@required this.count}) : assert(count != null);
@@ -57,13 +109,8 @@ class _$_Counter1 with DiagnosticableTreeMixin implements _Counter1 {
       runtimeType.hashCode ^ const DeepCollectionEquality().hash(count);
 
   @override
-  _$_Counter1 copyWith({
-    Object count = freezed,
-  }) {
-    return _$_Counter1(
-      count: count == freezed ? this.count : count as int,
-    );
-  }
+  _$Counter1CopyWith<_Counter1> get copyWith =>
+      __$Counter1CopyWithImpl<_Counter1>(this, _$identity);
 }
 
 abstract class _Counter1 implements Counter1 {
@@ -71,20 +118,14 @@ abstract class _Counter1 implements Counter1 {
 
   @override
   int get count;
-
   @override
-  _Counter1 copyWith({int count});
-}
-
-mixin _$Counter2 {
-  int get count;
-
-  Counter2 copyWith({int count});
+  _$Counter1CopyWith<_Counter1> get copyWith;
 }
 
 class _$Counter2TearOff {
   const _$Counter2TearOff();
 
+// ignore: unused_element
   _Counter2 call({@required int count}) {
     return _Counter2(
       count: count,
@@ -92,7 +133,62 @@ class _$Counter2TearOff {
   }
 }
 
+// ignore: unused_element
 const $Counter2 = _$Counter2TearOff();
+
+mixin _$Counter2 {
+  int get count;
+
+  $Counter2CopyWith<Counter2> get copyWith;
+}
+
+abstract class $Counter2CopyWith<$Res> {
+  factory $Counter2CopyWith(Counter2 value, $Res Function(Counter2) then) =
+      _$Counter2CopyWithImpl<$Res>;
+  $Res call({int count});
+}
+
+class _$Counter2CopyWithImpl<$Res> implements $Counter2CopyWith<$Res> {
+  _$Counter2CopyWithImpl(this._value, this._then);
+
+  final Counter2 _value;
+  // ignore: unused_field
+  final $Res Function(Counter2) _then;
+
+  @override
+  $Res call({
+    Object count = freezed,
+  }) {
+    return _then(_value.copyWith(
+      count: count == freezed ? _value.count : count as int,
+    ));
+  }
+}
+
+abstract class _$Counter2CopyWith<$Res> implements $Counter2CopyWith<$Res> {
+  factory _$Counter2CopyWith(_Counter2 value, $Res Function(_Counter2) then) =
+      __$Counter2CopyWithImpl<$Res>;
+  @override
+  $Res call({int count});
+}
+
+class __$Counter2CopyWithImpl<$Res> extends _$Counter2CopyWithImpl<$Res>
+    implements _$Counter2CopyWith<$Res> {
+  __$Counter2CopyWithImpl(_Counter2 _value, $Res Function(_Counter2) _then)
+      : super(_value, (v) => _then(v as _Counter2));
+
+  @override
+  _Counter2 get _value => super._value as _Counter2;
+
+  @override
+  $Res call({
+    Object count = freezed,
+  }) {
+    return _then(_Counter2(
+      count: count == freezed ? _value.count : count as int,
+    ));
+  }
+}
 
 class _$_Counter2 with DiagnosticableTreeMixin implements _Counter2 {
   const _$_Counter2({@required this.count}) : assert(count != null);
@@ -126,13 +222,8 @@ class _$_Counter2 with DiagnosticableTreeMixin implements _Counter2 {
       runtimeType.hashCode ^ const DeepCollectionEquality().hash(count);
 
   @override
-  _$_Counter2 copyWith({
-    Object count = freezed,
-  }) {
-    return _$_Counter2(
-      count: count == freezed ? this.count : count as int,
-    );
-  }
+  _$Counter2CopyWith<_Counter2> get copyWith =>
+      __$Counter2CopyWithImpl<_Counter2>(this, _$identity);
 }
 
 abstract class _Counter2 implements Counter2 {
@@ -140,7 +231,6 @@ abstract class _Counter2 implements Counter2 {
 
   @override
   int get count;
-
   @override
-  _Counter2 copyWith({int count});
+  _$Counter2CopyWith<_Counter2> get copyWith;
 }
