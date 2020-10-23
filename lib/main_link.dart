4a41621e7b629f2345a1329d56ee779cebc95c88
Masayuki Ono <mono0926@gmail.com>
2020-10-23 10:53:41 +0900

    Add main_link

---
+++ lib/main_link.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomePage(),
+      onGenerateRoute: (settings) {
+        if (settings.name == '/next') {
+          return MaterialPageRoute<dynamic>(
+            builder: (context) => Scaffold(
+              appBar: AppBar(),
+            ),
+            settings: settings,
+          );
+        }
+        return null;
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Link(
+              uri: Uri.parse('https://mono0926.com'),
+              builder: (context, followLink) => ElevatedButton(
+                child: const Text('mono'),
+                onPressed: followLink,
+              ),
+            ),
+            Link(
+              uri: Uri.parse('/next'),
+              builder: (context, followLink) => ElevatedButton(
+                child: const Text('next'),
+                onPressed: followLink,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
