2d1b2b5af32b0c07bea0d66a9cc8e5ac2749fe53
Masayuki Ono <mono0926@gmail.com>
2022-03-11 12:50:19 +0900

    Update platforms

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/mono/fvm/versions/stable"
+export "FLUTTER_APPLICATION_PATH=/Users/mono/Git/flutter_playground"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/mono/Git/flutter_playground/lib/main_complicated_scroll.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "EXCLUDED_ARCHS=arm64"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/mono/Git/flutter_playground/.dart_tool/package_config.json"
