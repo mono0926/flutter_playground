9dd821385e5a6497304695c8b8dc44e20daa7617
Masayuki Ono <mono0926@gmail.com>
2022-04-20 19:50:52 +0900

    Fix error

--- lib/main_deep_link.dart
+++ lib/main_deep_link.dart
@@ -1,5 +1,6 @@
 import 'package:animations/animations.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:collection/collection.dart';
 import 'package:faker/faker.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
