c39ff69e5c72fd77c04c0d2d593b831dce0f0058
Masayuki Ono <mono0926@gmail.com>
2022-04-27 14:06:24 +0900

    Update auto generated code

--- lib/main_deep_link.g.dart
+++ lib/main_deep_link.g.dart
@@ -3,6 +3,63 @@
 part of 'main_deep_link.dart';
 
 // **************************************************************************
+// GoRouterGenerator
+// **************************************************************************
+
+List<GoRoute> get $appRoutes => [
+      $homeRoute,
+    ];
+
+GoRoute get $homeRoute => GoRouteData.$route(
+      path: '/',
+      factory: $HomeRouteExtension._fromState,
+      routes: [
+        GoRouteData.$route(
+          path: 'users',
+          factory: $UsersRouteExtension._fromState,
+          routes: [
+            GoRouteData.$route(
+              path: ':userId',
+              factory: $UserRouteExtension._fromState,
+            ),
+          ],
+        ),
+      ],
+    );
+
+extension $HomeRouteExtension on HomeRoute {
+  static HomeRoute _fromState(GoRouterState state) => const HomeRoute();
+
+  String get location => GoRouteData.$location(
+        '/',
+      );
+
+  void go(BuildContext buildContext) => buildContext.go(location, extra: this);
+}
+
+extension $UsersRouteExtension on UsersRoute {
+  static UsersRoute _fromState(GoRouterState state) => const UsersRoute();
+
+  String get location => GoRouteData.$location(
+        '/users',
+      );
+
+  void go(BuildContext buildContext) => buildContext.go(location, extra: this);
+}
+
+extension $UserRouteExtension on UserRoute {
+  static UserRoute _fromState(GoRouterState state) => UserRoute(
+        state.params['userId']!,
+      );
+
+  String get location => GoRouteData.$location(
+        '/users/${Uri.encodeComponent(userId)}',
+      );
+
+  void go(BuildContext buildContext) => buildContext.go(location, extra: this);
+}
+
+// **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
