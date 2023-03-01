1ca948a36bc511a6434e73d2669799aa1785e1b6
mono <mono0926@gmail.com>
2023-03-01 17:35:49 +0900

    Update generated code

--- lib/main_riverpod_generator.g.dart
+++ lib/main_riverpod_generator.g.dart
@@ -8,7 +8,21 @@ part of 'main_riverpod_generator.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-// ignore_for_file: avoid_private_typedef_functions, non_constant_identifier_names, subtype_of_sealed_class, invalid_use_of_internal_member, unused_element, constant_identifier_names, unnecessary_raw_strings, library_private_types_in_public_api
+String _$helloWorldHash() => r'a45c9ef071dcfe251a00c73da58374aed6345624';
+
+/// See also [_helloWorld].
+@ProviderFor(_helloWorld)
+final _helloWorldProvider = AutoDisposeProvider<String>.internal(
+  _helloWorld,
+  name: r'_helloWorldProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$helloWorldHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _HelloWorldRef = AutoDisposeProviderRef<String>;
+String _$monoProviderHash() => r'ce687169279511a7fd843564a4f57a4f23b9fcf9';
 
 /// Copied from Dart SDK
 class _SystemHash {
@@ -31,23 +45,56 @@ class _SystemHash {
   }
 }
 
-String _$_helloWorldHash() => r'a45c9ef071dcfe251a00c73da58374aed6345624';
+typedef _MonoProviderRef = AutoDisposeFutureProviderRef<String>;
 
-/// See also [_helloWorld].
-final _helloWorldProvider = AutoDisposeProvider<String>(
-  _helloWorld,
-  name: r'_helloWorldProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$_helloWorldHash,
-);
-typedef _HelloWorldRef = AutoDisposeProviderRef<String>;
-String _$_monoProviderHash() => r'ce687169279511a7fd843564a4f57a4f23b9fcf9';
+/// See also [_monoProvider].
+@ProviderFor(_monoProvider)
+const _monoProviderProvider = _MonoProviderFamily();
+
+/// See also [_monoProvider].
+class _MonoProviderFamily extends Family<AsyncValue<String>> {
+  /// See also [_monoProvider].
+  const _MonoProviderFamily();
+
+  /// See also [_monoProvider].
+  _MonoProviderProvider call(
+    String arg,
+  ) {
+    return _MonoProviderProvider(
+      arg,
+    );
+  }
+
+  @override
+  _MonoProviderProvider getProviderOverride(
+    covariant _MonoProviderProvider provider,
+  ) {
+    return call(
+      provider.arg,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'_monoProviderProvider';
+}
 
 /// See also [_monoProvider].
 class _MonoProviderProvider extends AutoDisposeFutureProvider<String> {
+  /// See also [_monoProvider].
   _MonoProviderProvider(
     this.arg,
-  ) : super(
+  ) : super.internal(
           (ref) => _monoProvider(
             ref,
             arg,
@@ -57,7 +104,10 @@ class _MonoProviderProvider extends AutoDisposeFutureProvider<String> {
           debugGetCreateSourceHash:
               const bool.fromEnvironment('dart.vm.product')
                   ? null
-                  : _$_monoProviderHash,
+                  : _$monoProviderHash,
+          dependencies: _MonoProviderFamily._dependencies,
+          allTransitiveDependencies:
+              _MonoProviderFamily._allTransitiveDependencies,
         );
 
   final String arg;
@@ -75,38 +125,4 @@ class _MonoProviderProvider extends AutoDisposeFutureProvider<String> {
     return _SystemHash.finish(hash);
   }
 }
-
-typedef _MonoProviderRef = AutoDisposeFutureProviderRef<String>;
-
-/// See also [_monoProvider].
-final _monoProviderProvider = _MonoProviderFamily();
-
-class _MonoProviderFamily extends Family<AsyncValue<String>> {
-  _MonoProviderFamily();
-
-  _MonoProviderProvider call(
-    String arg,
-  ) {
-    return _MonoProviderProvider(
-      arg,
-    );
-  }
-
-  @override
-  AutoDisposeFutureProvider<String> getProviderOverride(
-    covariant _MonoProviderProvider provider,
-  ) {
-    return call(
-      provider.arg,
-    );
-  }
-
-  @override
-  List<ProviderOrFamily>? get allTransitiveDependencies => null;
-
-  @override
-  List<ProviderOrFamily>? get dependencies => null;
-
-  @override
-  String? get name => r'_monoProviderProvider';
-}
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
